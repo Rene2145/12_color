085c07a802156c16bb6816f7e958c33e2061b0ef
TheFallenUSER <eduardopea616@gmail.com>
2025-06-16 16:26:43 -0500

    Añadiendo carpeta desafio1 de la clase 12 de PG

---
+++ desafio1/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import ColorSensor
+from pybricks.parameters import Color, Port
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+
+ev3 = EV3Brick()
+color_sensor = ColorSensor(Port.S1)
+
+while True:
+    detected_color = color_sensor.color()
+
+    ev3.screen.clear()
+
+    if detected_color == Color.RED:
+        ev3.screen.print("Color detectado:")
+        ev3.screen.print("Rojo")   
+
+        wait(5000)
+        
+    else:
+        ev3.screen.print("Desconocido")
+
+        wait(5000)
